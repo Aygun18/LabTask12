4a2758678c0d6cb00f900b68fe9fbfd09579d921
Aygun <sahmuradova18@gmail.com>
2024-03-19 11:46:17 +0400

    LabTask12

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+
+namespace LabTask12
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Ededi daxil edin: ");
+			int num= Convert.ToInt32(Console.ReadLine());
+			GetNum(num);
+
+        }
+		static void GetNum(int num)
+		{
+
+			int num1=0;
+
+			while (num>0)
+			{
+				//num1=num%10;
+				//Console.WriteLine(num1);
+				//num /= 10;
+				num1 = num1 * 10;
+				num1=num1+num%10;
+				num = num / 10;
+			}
+			Console.WriteLine(num1);
+           
+        }
+	}
+}
